70bcde435687bb7ea0fa18840466ae112d8b9e0b
rctraining <33338434+rctraining@users.noreply.github.com>
2018-10-09 11:06:55 -0600

    Add files via upload

---
+++ Bash_scripting/function.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+# echo back $1 and $2, arguments to this script.
+
+e () {
+echo $1;
+}
+
+#now test
+e $1
+e $2
---
+++ Bash_scripting/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /tmp
+pwd
+ls |tail -n10
+# cd - # not really needed, why?
