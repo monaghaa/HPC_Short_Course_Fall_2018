b723bb2bc521df472c0cc675ed05872b3e8642cb
Shelley Knuth <shelley.knuth@colorado.edu>
2018-10-15 12:53:36 -0600

    Fixed loadbalance problem

--- Job_Submission/code/submit_loadbalance.sh
+++ Job_Submission/code/submit_loadbalance.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --ntasks=2
+#SBATCH --ntasks=4
 #SBATCH --time=00:02:00
 #SBATCH --qos=testing
 #SBATCH --partition=shas-testing
 #SBATCH --output=loadbalance_%j.out
-#SBATCH --reservation=tutorial1  
+###SBATCH --reservation=tutorial1  
 
 # Written by:    Andrew Monaghan, 08 March 2018
 # Purpose:     To demonstrate how to submit several jobs using the loadbalance tool
 
 module purge
- 
+
 module load intel
 module load impi
-module load python/2.7.11
+module load python
 module load loadbalance
 
-mpirun lb cmd_file
+mpirun lb lb_cmd_file
+
